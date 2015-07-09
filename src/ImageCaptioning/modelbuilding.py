9a900403d4d2ee294fcd57f6feb62044da40c68a
youralien <ryan.louie@students.olin.edu>
2015-07-09 16:50:37 -0400

    ADD: working cost pairwise cost function w/ tests

--- src/ImageCaptioning/modelbuilding.py
+++ src/ImageCaptioning/modelbuilding.py
@@ -46,13 +46,14 @@ class Encoder(Initializable):
         
         image_embedding = self.image_embedding.apply(image_vects)
         
-        inputs = self.to_inputs.apply(word_vects)
-        hidden, cells = self.transition.apply(inputs=inputs, mask=None)
+        # inputs = self.to_inputs.apply(word_vects)
+        inputs = word_vects.dimshuffle(1, 0, 2)
+        # hidden, cells = self.transition.apply(inputs=inputs, mask=None)
 
-        # the last hidden state represents the accumulation of all the words (i.e. the sentence)
-        # grab all batches, grab the last value representing accumulation of the sequence, grab all features
-        sentence_embedding = hidden[:,-1, :]
-
+        # # the last hidden state represents the accumulation of all the words (i.e. the sentence)
+        # # grab all batches, grab the last value representing accumulation of the sequence, grab all features
+        # sentence_embedding = hidden[-1]
+        sentence_embedding = inputs.mean(axis=0)
         return image_embedding, sentence_embedding
 
 @add_metaclass(ABCMeta)
@@ -95,45 +96,151 @@ class PairwiseRanking(PairwiseCostMatrix):
             )
 
         # cannot be broadcasted together if not transposed
-        cost = x_cost + v_cost.T
+        cost = x_cost + v_cost
 
         return cost
 
+# l2 norm, row-wise
+def l2norm(X):
+    norm = tensor.sqrt(tensor.pow(X, 2).sum(1))
+    X /= norm[:, None]
+    return X
+
 def cos_sim(x, v):
-    scaled_x = x / tensor.nlinalg.norm(x, ord=1)
-    scaled_v = v / tensor.nlinalg.norm(x, ord=1)
+    """
+    Parameters
+    ----------
+    x shape (batch_size, dim)
+    v shape (batch_size, dim)
+
+    Intermediate
+    ------------
+    cosine_similarity (batch_size, batch_size)
+        the cos_sim of batch i of X with batch j of V
+    
+    Returns
+    -------
+    cosine_simliarity_matching (batch_size, )
+        the cos_sim of batch i of X and i of V
+    """
+
+    scaled_x = l2norm(x)
+    scaled_v = l2norm(v)
+    # scaled_x = x / tensor.nlinalg.norm(x, None)
+    # scaled_v = v / tensor.nlinalg.norm(x, None)
 
     # cannot be dotted together if not transposed
     cosine_similarity = tensor.dot(scaled_x, scaled_v.T)
 
-    return cosine_similarity
+    cosine_similarity_matching = cosine_similarity.diagonal()
+    return cosine_similarity_matching
 
 if __name__ == '__main__':
     import theano
     import theano.tensor as T
-    theano.config.compute_test_value = 'warn'
-
-    image_vects = T.matrix('image_vects')
-    word_vects = T.tensor3('word_vects')
-    
     import numpy as np
 
-    batch_size = 2
-    image_feature_dim = 64
-    seq_len = 4
-    embedding_dim = 7
-    image_vects.tag.test_value = np.zeros((batch_size, image_feature_dim), dtype='float32')
-    word_vects.tag.test_value = np.zeros((batch_size, seq_len, embedding_dim), dtype='float32')
+    def test_encoder():
+        image_vects = T.matrix('image_vects')
+        word_vects = T.tensor3('word_vects')
+        
 
-    s = Encoder(
-              image_feature_dim=image_feature_dim
-            , embedding_dim=embedding_dim
-            , biases_init=Constant(0.)
-            , weights_init=IsotropicGaussian(0.02)
-            )
-    s.initialize()
-    iem, sem = s.apply(image_vects, word_vects)
+        batch_size = 2
+        image_feature_dim = 64
+        seq_len = 4
+        embedding_dim = 7
 
-    # expecting sentence embedding to be [batch_size, embedding_dim]
-    f = theano.function([word_vects], sem)
-    print(f(word_vects.tag.test_value))
+        s = Encoder(
+                  image_feature_dim=image_feature_dim
+                , embedding_dim=embedding_dim
+                , biases_init=Constant(0.)
+                , weights_init=IsotropicGaussian(0.02)
+                )
+        s.initialize()
+        iem, sem = s.apply(image_vects, word_vects)
+
+        image_vects_tv = np.zeros((batch_size, image_feature_dim), dtype='float32')
+        word_vects_tv = np.zeros((batch_size, seq_len, embedding_dim), dtype='float32')
+        # expecting sentence embedding to be [batch_size, embedding_dim]
+        f = theano.function([word_vects], sem)
+        print(f(word_vects_tv))
+
+    def test_cos_sim():
+        X = T.matrix('iem')
+        V = T.matrix('sem')
+
+        s = cos_sim(X, V)
+
+        f = theano.function([X, V], s, allow_input_downcast=True)
+
+        batch_size = 3
+        embedding_dim = 7
+        shape = (batch_size, embedding_dim)
+
+        print "\n each x, v in X,V are similar, cost should be low"
+        X_tv = np.random.rand(*shape)
+        V_tv = X_tv.copy()
+
+        print "in: x, v"
+        print X_tv
+        print V_tv
+        print "out: cos sim"
+        print f(X_tv, V_tv)
+
+    def test_cost():
+
+        margin = 0.2 # alpha term, should not be more than 1!
+        batch_size = 5
+        embedding_dim = 50
+        shape = (batch_size, embedding_dim)
+
+        # image embedding
+        lim = T.matrix('lim')
+        lcim = T.matrix('lcim')
+
+        # sentence embedding
+        ls = T.matrix('ls')
+        lcs = T.matrix('lcs')
+
+        # l2norms
+        lim = l2norm(lim)
+        lcim = l2norm(lcim)
+        ls = l2norm(ls)
+        lcs = l2norm(lcs)
+
+        # tile by number of contrastive terms
+        lim = tensor.tile(lim, (batch_size, 1))
+        ls = tensor.tile(ls, (batch_size, 1))
+        
+        # pairwise ranking loss (https://github.com/youralien/skip-thoughts/blob/master/eval_rank.py)
+        cost_im = margin - (lim * ls).sum(axis=1) + (lim * lcs).sum(axis=1)
+        cost_im = cost_im * (cost_im > 0.) # this is like the max(0, pairwise-ranking-loss)
+        cost_im = cost_im.sum(0)
+
+        cost_s = margin - (ls * lim).sum(axis=1) + (ls * lcim).sum(axis=1)
+        cost_s = cost_s * (cost_s > 0.) # this is like max(0, pairwise-ranking-loss)
+        cost_s = cost_s.sum(0)
+
+        cost = cost_im + cost_s
+        # cost = PairwiseRanking(alpha=margin).apply(X, V, X_k, V_k)
+
+        f = theano.function([lim, ls, lcim, lcs], cost, allow_input_downcast=True)
+
+
+        print "\n each x, v in X,V are similar, cost should be low"
+        X_tv = np.random.rand(*shape)
+        V_tv = X_tv.copy()
+
+        X_k_tv = np.random.rand(*shape)
+        V_k_tv = np.random.rand(*shape)
+        print "cost: ",f(X_tv, V_tv, X_k_tv, V_k_tv)
+
+        print "\n each x, v in X,V are dissimlar, cost should be high"
+        X_tv = np.random.rand(*shape)
+        V_tv = np.random.rand(*shape)
+
+        X_k_tv = np.random.rand(*shape)
+        V_k_tv = np.random.rand(*shape)
+        print "cost: ",f(X_tv, V_tv, X_k_tv, V_k_tv)
+
+    test_cost()
