620e5c825a32ae97ea4dfdfce7755f100584b56a
youralien <ryan.louie@students.olin.edu>
2015-07-10 19:07:13 -0400

    ADD: ModelIO and ModelEval

--- src/ImageCaptioning/caption.py
+++ src/ImageCaptioning/caption.py
@@ -1,4 +1,5 @@
 # scientific python
+import numpy as np
 import theano
 from theano import tensor as T
 
@@ -33,10 +34,10 @@ from dataset import (coco, FoxyDataStream, GloveTransformer,
 # DataPreprocessing #
 # # # # # # # # # # #
 batch_size = 128
-trX, teX, trY, teY = coco(mode="dev", batch_size=batch_size)
-trX_k, teX_k, trY_k, teY_k = coco(mode="dev", batch_size=batch_size)
+trX, teX, trY, teY = coco(mode="dev", batch_size=batch_size, n_captions=3)
+trX_k, teX_k, trY_k, teY_k = (trX, teX, trY, teY)
+# trX_k, teX_k, trY_k, teY_k = coco(mode="dev", batch_size=batch_size, )
 # trX, teX, trY, teY = coco(mode='everything')
-# trX_k, teX_k, trY_k, teY_k = (trX, teX, trY, teY)
 
 # trX_k, teX_k, trY_k, teY_k = coco(mode="dev", batch_size=batch_size)
 
@@ -158,7 +159,6 @@ word_vects = T.tensor3('word_vects') # named to match the source name
 image_vects_k = T.matrix('image_vects_k') # named to match the contrastive source name
 word_vects_k = T.tensor3('word_vects_k') # named to match the contrastive source name
 
-# import numpy as np
 # image_vects.tag.test_value = np.zeros((2, 4096), dtype='float32')
 # word_vects.tag.test_value = np.zeros((2, 15, 50), dtype='float32')
 # image_vects_k.tag.test_value = np.zeros((2, 4096), dtype='float32')
@@ -195,8 +195,13 @@ cost_s = cost_s.sum(0)
 cost = cost_im + cost_s
 cost.name = "pairwise_ranking_loss"
 
+# function to produce embedding
+f_emb = theano.function([image_vects, word_vects], [lim, ls])
+
 cg = ComputationGraph(cost)
 
+import ipdb
+ipdb.set_trace()
 # # # # # # # # # # #
 # Modeling Training #
 # # # # # # # # # # #
@@ -221,7 +226,103 @@ main_loop = MainLoop(
             , prefix='test')
         , ProgressBar()
         , Printing()
-        # , FinishAfter(after_n_epochs=10)
+        , FinishAfter(after_n_epochs=15)
         ]
     )
 main_loop.run()
+
+# # # # # # #
+#  Model IO #
+# # # # # # #
+
+import cPickle as pkl
+
+class ModelIO():
+
+    @staticmethod
+    def save(func, saveto):
+        pkl.dump(func, open('%s.pkl'%saveto, 'wb'))
+
+    @staticmethod
+    def load(saveto):
+        func = pkl.load(open('%s.pkl'%saveto, 'r'))
+        return func
+
+ModelIO.save(f_emb, '/home/luke/datasets/coco/predict/encoder')
+# f_emb = ModelIO.load('/home/luke/datasets/coco/predict/encoder')
+
+# # # # # # # # # # #
+# Model Evaluation  #
+# # # # # # # # # # #
+
+class ModelEval():
+
+    @staticmethod
+    def captions(filenames, top_n=3):
+        image_features, captions = coco(filenames)
+
+    @staticmethod
+    def rankscores(final_train_stream, final_test_stream, f_emb):
+
+        i2t = ModelEval.i2t
+        train_ep = final_train_stream.get_epoch_iterator()
+        test_ep = final_test_stream.get_epoch_iterator()
+
+        train_metrics = []
+        test_metrics = []
+        for train_data, test_data in train_ep, test_ep:
+            im_emb, s_emb = f_emb(*train_data)
+            train_metrics.append(i2t(im_emb, s_emb))
+            im_emb, s_emb = f_emb(*train_data)
+            test_metrics.append(i2t(im_emb, s_emb))
+        train_metrics = np.vstack(train_metrics)
+        test_metrics = np.vstack(test_metrics)
+        
+        metric_names = ("r1", "r5", "r10", "med")
+        print "\nMean Metric Scores:"
+        for i, metric_name in enumerate(metric_names):
+            for metrics in (train_metrics, test_metrics):
+                print "%s: %d" % metric_name, np.mean(metrics[:, i])
+        
+        return train_metrics, test_metrics
+
+    @staticmethod
+    def i2t(images, captions, z=1, npts=None):
+        """
+        Images: (z*N, K) matrix of images
+        Captions: (z*N, K) matrix of captions
+        """
+        if npts == None:
+            npts = images.shape[0] / z
+        index_list = []
+
+        # Project captions
+        for i in range(len(captions)):
+            captions[i] /= np.linalg.norm(captions[i])
+
+        ranks = np.zeros(npts)
+        for index in range(npts):
+
+            # Get query image
+            im = images[z * index].reshape(1, images.shape[1])
+            im /= np.linalg.norm(im)
+
+            # Compute scores
+            d = np.dot(im, captions.T).flatten()
+            inds = np.argsort(d)[::-1]
+            index_list.append(inds[0])
+
+            # Score
+            rank = 1e20
+            for i in range(z*index, z*index + z, 1):
+                tmp = np.where(inds == i)[0][0]
+                if tmp < rank:
+                    rank = tmp
+            ranks[index] = rank
+
+        # Compute metrics
+        r1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
+        r5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
+        r10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
+        medr = np.floor(np.median(ranks)) + 1
+        return (r1, r5, r10, medr)
