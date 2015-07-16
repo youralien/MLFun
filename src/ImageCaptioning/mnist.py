384f92be7efb8fd774420aa8f40e1e4086c8fd34
youralien <ryan.louie@students.olin.edu>
2015-07-16 17:26:32 -0400

    ADD: model save and load successful, pkl magic

--- src/ImageCaptioning/mnist.py
+++ src/ImageCaptioning/mnist.py
@@ -101,87 +101,6 @@ from blocks.monitoring import aggregation
 from blocks.graph import ComputationGraph
 from modelbuilding import GatedRecurrentWithInitialState
 
-def l2(arr, axis=None):
-    """Return the L2 norm of a tensor.
-    Parameters
-    ----------
-    arr : Theano variable.
-        The variable to calculate the norm of.
-    axis : integer, optional [default: None]
-        The sum will be performed along this axis. This makes it possible to
-        calculate the norm of many tensors in parallel, given they are organized
-        along some axis. If not given, the norm will be computed for the whole
-        tensor.
-    Returns
-    -------
-    res : Theano variable.
-        If ``axis`` is ``None``, this will be a scalar. Otherwise it will be
-        a tensor with one dimension less, where the missing dimension
-        corresponds to ``axis``.
-    Examples
-    --------
-    >>> from theano.printing import pprint
-    >>> v = T.vector()
-    >>> this_norm = l2(v)
-    >>> pprint(this_norm)
-    'sqrt((Sum((<TensorType(float32, vector)> ** TensorConstant{2})) + TensorConstant{9.99999993923e-09}))'
-    >>> m = T.matrix()
-    >>> this_norm = l2(m, axis=1)
-    >>> pprint(this_norm)
-    'sqrt((Sum{1}((<TensorType(float32, matrix)> ** TensorConstant{2})) + TensorConstant{9.99999993923e-09}))'
-    >>> m = T.matrix()
-    >>> this_norm = l2(m)
-    >>> pprint(this_norm)
-    'sqrt((Sum((<TensorType(float32, matrix)> ** TensorConstant{2})) + TensorConstant{9.99999993923e-09}))'
-    """
-    return T.sqrt((arr ** 2).sum(axis=axis) + 1e-8)
-
-class SimilarityEmitter(AbstractEmitter):
-    """An emitter for the trivial case when readouts are outputs.
-    Parameters
-    ----------
-    readout_dim : int
-        The dimension of the readout.
-    Notes
-    -----
-    By default :meth:`cost` always returns zero tensor.
-    """
-    @lazy(allocation=['readout_dim'])
-    def __init__(self, readout_dim, **kwargs):
-        super(SimilarityEmitter, self).__init__(**kwargs)
-        self.readout_dim = readout_dim
-
-    @application
-    def emit(self, readouts):
-        return readouts
-
-    @application
-    def cost(self, readouts, outputs):
-        l2readouts = l2(readouts)
-        l2outputs = l2(outputs)
-        readouts = readouts // l2readouts
-        outputs = outputs // l2outputs
-
-        # cosine similarity cost.  if the vectors predicted are the actual ones
-        # cos_sim will be 1, 1 -1 = 0 which is 0 cost.
-        margin = 1 # alpha term, should not be more than 1!
-
-        # pairwise ranking loss (https://github.com/youralien/skip-thoughts/blob/master/eval_rank.py)
-        cost = margin - (readouts * outputs).mean(axis=1)
-        cost = cost * (cost > 0.) # this is like the max(0, pairwise-ranking-loss)
-        cost = cost.mean(0)
-        cost.name = "similarity"
-        return cost
-
-    @application
-    def initial_outputs(self, batch_size):
-        return T.zeros((batch_size, self.readout_dim))
-
-    def get_dim(self, name):
-        if name == 'outputs':
-            return self.readout_dim
-        return super(SimilarityEmitter, self).get_dim(name)
-
 class MNISTPoet(Initializable):
 
     def __init__(self, image_dim, dim, alphabet_size=26, **kwargs):
@@ -311,12 +230,28 @@ def main(mode, save_path, num_batches=300):
         sample = mnistpoet.generate(im)
         #f_gen = theano.function([im], sample)
         f_gen = ComputationGraph(sample).get_theano_function()
+        try:
+            ModelIO.save(f_gen, 'predict-mnist/f_gen')
+            print "It actually saved.  Thank you pickle!"
+        except Exception, e:
+            print "Fuck Pickle and continue :)"
+            print e
+        ModelEval.predict(f_gen)
+    else:
+        logger.info("Loading Model ...")
+        f_gen = ModelIO.load('predict-mnist/f_gen')
+        ModelEval.predict(f_gen)
+
+class ModelEval():
+
+    @staticmethod
+    def predict(f_gen):
         ep = getTestStream(batch_size=1).get_epoch_iterator()
         while True:
             im_vects, txt_enc = ep.next()
             mnist_txt = "".join(code2char[code] for code in txt_enc[0])
             print "\nTrying for: ", mnist_txt
-            message=("Number Tries to generate correct text?")
+            message=("Number of attempts to generate correct text? ")
             batch_size = int(input(message))
             states, outputs, costs = f_gen(
                     np.repeat(im_vects, batch_size, 0)
@@ -341,92 +276,10 @@ def main(mode, save_path, num_batches=300):
             messages.sort(key=operator.itemgetter(0), reverse=True)
             for _, message in messages:
                 print(message)
-        ModelIO.save(f_gen, 'predict-mnist/f_gen')
-#    elif mode == "sample" or mode == "beam_search" or mode == "generate":
-#        im = T.matrix('features')
-#        #mnistpoet.initialize()
-#        generated = mnistpoet.generate(im)
-#        model = Model(generated)
-#        logger.info("Loading the Model...")
-#        pkl.load(open(save_path), 'rb')
-#        #model.set_parameter_values(load_parameter_values(save_path))
-#
-#        def generate(input_):
-#            """Generate ouptut sequences for a given image
-#
-#            Returns
-#            -------
-#            outputs: list of lists
-#                Trimmed output sequences
-#            costs: list
-#                The negative log-likelihood of generating the respective
-#                sequences.
-#            """
-#            if mode == "beam_search":
-#                print "No beamsearch available"
-#                #samples, = VariableFilter(
-#                #    bricks=[mnistpoet.generator], name="outputs")(
-#                #        ComputationGraph(generated[1]))
-#                #beam_search = BeamSearch(samples)
-#                ## confused if chars: should be input_ or input_ should image
-#                #outputs, costs = beam_search.search(
-#                #    {chars: input_i
-#            else:
-#                import ipdb; ipdb.set_trace();
-#                _1, outputs, _2, _3, costs = (
-#                    model.get_theano_function()(input_))
-#                outputs = list(outputs.T)
-#                costs = list(costs.T)
-#                for i in range(len(outputs)):
-#                    outputs[i] = list(outputs[i])
-#                    try:
-#                        # 0 was my stop character for MNIST alphabetic characters
-#                        true_length = outputs[i].index(0)
-#                    except ValueError:
-#                        true_length = len(outputs[i])
-#                    # true length helps me 'mask' stop characters in the cost calc
-#                    outputs[i] = outputs[i][:true_length]
-#                    costs[i] = costs[i][:true_length].sum()
-#            return outputs, costs
-#        #main_loop = pkl.load(open(save_path, 'rb'))
-#        #generator = main_loop.model
-#        f_gen = ModelIO.load('predict-mnist/f_gen')
-#        # get one example at a time to evaluate
-#        ep = getTestStream(batch_size=1).get_epoch_iterator()
-#
-#        while True:
-#            # shape (1, image_features), (1, character sequence length)
-#            mnist_im, mnist_txt_enc = ep.next()
-#            # get the first one
-#            #mnist_im = mnist_im[0]
-#            mnist_txt_enc = mnist_txt_enc[0]
-#            mnist_txt = "".join(code2char[code] for code in mnist_txt_enc)
-#            print "Target: ", mnist_txt
-#            message = ("Enter the number of samples\n" if mode == "sample"
-#                        else "Enter the beam size\n")
-#            batch_size = int(input(message))
-#            f_gen_out = f_gen(mnist_im)
-#            #sample = ComputationGraph(generator.generate(mnist_im)).get_theano_function()
-#            #states, outputs, costs = [data[:, 0] for data in sample()]
-#
-#            import ipdb; ipdb.set_trace();
-#            samples, costs = generate(
-#                    np.repeat(np.array(mnist_im),
-#                          batch_size, axis=0)
-#                    )
-#            messages = []
-#            for sample, cost in equizip(samples, costs):
-#                message = "({})".format(cost)
-#                message += "".join(code2char[code] for code in sample)
-#                messages.append((cost, message))
-#            messages.sort(key=operator.itemgetter(0), reverse=True)
-#            for _, message in messages:
-#                print(message)
 
 # # # # # # #
 #  Model IO #
 # # # # # # #
-
 
 class ModelIO():
 
@@ -440,6 +293,6 @@ class ModelIO():
         return func
 
 if __name__ == "__main__":
-    main("train", "predict-mnist/pklmain_loop")
+    #main("train", "predict-mnist/pklmain_loop")
     main("sample", "predict-mnist/pklmain_loop")
 
